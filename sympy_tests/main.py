86511aabfda70eac43197dc5e990f8fdcfa70673
Mustapha S. Rashiduddin <saif.rashiduddin@gmail.com>
2022-10-16 14:34:50 +0500

    modified zhou's code and renamed file

--- sympy_tests/main.py
+++
@@ -1,8 +0,0 @@
-# export to professor's code
-
-from numpy import pi
-from numpy import e
-
-def optimum_weight(M, tau, R, eta, lmd, T, theta, t):
-    return pi*M*e*(T*tau - t*tau)/R + eta*(M*e*(T*tau - t*tau)/R - 1 - 
-                                           e*(0.5*T*eta*theta**T - 0.5*eta*t*theta**T)/(R*lmd))
